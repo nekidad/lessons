708f94a162373612c7d6a9266de10bb37434d66b
nekidad <nekidad@MacBook-Pro-nekidad.local>
2022-09-04 22:48:31 +0300

    new4september

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+/* "use strict"; */
 /* console.log(1); */
-"use strict";
 /* alert("Hello Nik");
 console.log(alert); */
 
@@ -14,7 +14,7 @@ answers[1] = prompt('Это новый кадилак?', 'Right'),
 answers[2] = prompt('Вам есть 18 лет?', 'Yes');
 
 console.log(answers); */
-const category = 'toys';
+/* const category = 'toys';
 console.log(`https://someurl.com/${category}/5`);
 
 const user = "Nikita";
@@ -29,6 +29,23 @@ console.log(2 + 2 * 2 != 8);
 const isChecked = false,
       isClose = false;
 
-console.log(isChecked || !isClose);
+console.log(isChecked || !isClose); */
+"use strict";
+const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const 
+const personalMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+      b = prompt('На сколько оцените его?', ""),
+      c = prompt('Один из последних просмотренных фильмов?', ""),
+      d = prompt('На сколько оцените его?', "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
